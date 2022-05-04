ac9f974d39900aa62549e1587c408e935cc8fcce
Shiva <shanshiva@outlook.com>
2022-05-05 01:45:29 +0530

    Added hours & minutes constants ⌚

--- backend/contracts/libraries/LibAppStorage.sol
+++ backend/contracts/libraries/LibAppStorage.sol
@@ -6,6 +6,8 @@ library AppConstants{
     uint256 constant isTestMode = 1;
     uint256 constant publicPrice = 1000000000000000; 
     uint256 constant dayInSeconds = 86400;
+    uint256 constant hourInSeconds = 3600;
+    uint256 constant minutesInSeconds = 60;
     uint256 constant _NOT_ENTERED = 1;
     uint256 constant _ENTERED = 2;
 }
