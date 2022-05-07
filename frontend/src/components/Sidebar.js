e6c94466639aecfc0a2c3472dd781f0f67d1ce47
Shiva <shanshiva@outlook.com>
2022-05-07 15:48:48 +0530

    Fancy Sidebar Added 🧩🔭

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -20,39 +20,41 @@ function Sidebar(props) {
                 <img src='/sidebar/logo2.png' alt='logo' className={!collapse ? 'proSidebar__header__logo' : 'proSidebar__header__logo img-size-75'} />
             </SidebarHeader>
             <SidebarContent>
+                
                 <Menu iconShape='circle'>
                     <MenuItem icon={<Icon icon="flat-color-icons:home" fontSize='20px' />} onClick={() => { navigate("/") }}>Home</MenuItem>
-                    <MenuItem icon={<Icon icon="noto:water-wave" fontSize='20px' />} onClick={() => { navigate("/wave") }}>WaveMe</MenuItem>
-                    
-                    
-                    <SubMenu title="NFT Game" icon={<Icon icon="noto:video-game" fontSize='20px' />} className="react-slidedown pro-inner-list-item">
+                    {/* <MenuItem icon={<Icon icon="noto:water-wave" fontSize='20px' />} onClick={() => { navigate("/wave") }}>WaveMe</MenuItem> */}
+                    <SubMenu 
+                    title="NFT Game" 
+                    icon={<Icon icon="noto:video-game" fontSize='20px' />} 
+                    >
 
                         <MenuItem icon={<Icon icon="noto:joystick" fontSize='20px' />} onClick={() => { navigate("/nftch") }}>Choose Character</MenuItem>
                         <MenuItem icon={<Icon icon="twemoji:crossed-swords" fontSize='20px' />} onClick={() => { navigate("/nftbattle") }}>Battle</MenuItem>
                         <MenuItem icon={<Icon icon="noto:bank" fontSize='20px' />} onClick={() => { navigate("/nftstake") }}>Stake</MenuItem>
 
-                        <SubMenu title="NFT Rental Market" icon={<Icon icon="emojione:department-store" fontSize='20px' />} className="react-slidedown pro-inner-list-item">
-                            <MenuItem icon={<Icon icon="emojione:department-store" fontSize='20px' />} onClick={() => { navigate("/rentalnfts") }}>Rental Market</MenuItem>
-                            <MenuItem icon={<Icon icon="emojione:admission-tickets" fontSize='20px' />} onClick={() => { navigate("/listnfts") }}>My Listings</MenuItem>
-                            <MenuItem icon={<Icon icon="twemoji:framed-picture" fontSize='20px' />} onClick={() => { navigate("/mynfts") }}>My NFTs</MenuItem>
-                            <MenuItem icon={<Icon icon="twemoji:money-with-wings" fontSize='20px' />} onClick={() => { navigate("/myrentals") }}>Rented NFTs</MenuItem>
-                            <MenuItem icon={<Icon icon="twemoji:hourglass-done" fontSize='20px' />} onClick={() => { navigate("/lentnfts") }}>Lent NFTs</MenuItem>
-                        </SubMenu>
-
-
                     </SubMenu>
 
-                    
+                    <SubMenu 
+                    title="NFT Rental Market" 
+                    icon={<Icon icon="emojione:department-store" fontSize='20px' />} 
+                    >
+                        <MenuItem icon={<Icon icon="emojione:department-store" fontSize='20px' />} onClick={() => { navigate("/rentalnfts") }}>Rental Market</MenuItem>
+                        <MenuItem icon={<Icon icon="emojione:admission-tickets" fontSize='20px' />} onClick={() => { navigate("/listnfts") }}>My Listings</MenuItem>
+                        <MenuItem icon={<Icon icon="twemoji:framed-picture" fontSize='20px' />} onClick={() => { navigate("/mynfts") }}>My NFTs</MenuItem>
+                        <MenuItem icon={<Icon icon="twemoji:money-with-wings" fontSize='20px' />} onClick={() => { navigate("/myrentals") }}>Rented NFTs</MenuItem>
+                        <MenuItem icon={<Icon icon="twemoji:hourglass-done" fontSize='20px' />} onClick={() => { navigate("/lentnfts") }}>Lent NFTs</MenuItem>
+                    </SubMenu>
+                </Menu>    
 
-                    
-                </Menu>
+                
             </SidebarContent>
             <SidebarFooter>
                 <Flex flexDirection={collapse ? 'column' : 'row'} justifyContent='center' alignItems='center' p='20px' mr={!collapse && '30px'}>
                     <Link href="https://github.com/ShivaShanmuganathan" mr={!collapse && '10px'} mb={collapse && '10px'}><Icon icon="bi:github" color="white" width="25" height="25" /></Link>
                     <Link href="https://www.linkedin.com/in/shiva-shanmuganathan/" mr={!collapse && '10px'} mb={collapse && '10px'}><Icon icon="bi:linkedin" color="white" width="25" height="25" /></Link>
                     <Link href="http://shiva-blockchain-portfolio.netlify.app/" mr={!collapse && '10px'} mb={collapse && '10px'}><Icon icon="ic:round-personal-video" color="white" width="25" height="25" /></Link>
-                    <Link href="https://discord.com/users/547758282941530127"><Icon icon="simple-icons:discord" color="white" width="25" height="25" /></Link>
+                    <Link href="https://twitter.com/shanshiva1"><Icon icon="simple-icons:twitter" color="white" width="25" height="25" /></Link>
                     
                     {collapse && (
                         <Button className='proSidebar__collapseBtn relative' onClick={() => setCollapse(!collapse)}>
